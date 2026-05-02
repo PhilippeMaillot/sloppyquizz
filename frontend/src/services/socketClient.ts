0671709feb38d3595d9b1242fdbac4d15db74e79
PhilippeMaillot <maillotphilippe78@gmail.com>
2026-05-02 20:39:53 +0200

    fix

--- frontend/src/services/socketClient.ts
+++ frontend/src/services/socketClient.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client'
 
-export const socketClient = io(import.meta.env.VITE_SOCKET_URL ?? 'http://localhost:8000', {
+const defaultSocketUrl = window.location.origin
+
+export const socketClient = io(import.meta.env.VITE_SOCKET_URL ?? defaultSocketUrl, {
   autoConnect: false,
   path: '/socket.io',
 })
