26865b0624c09607eed091bce1a1872721548aeb
PhilippeMaillot <maillotphilippe78@gmail.com>
2026-05-02 20:23:56 +0200

    config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'quiz.omnissh.app'
+    ],
     proxy: {
       '/uploads': {
         target: 'http://localhost:8000',
